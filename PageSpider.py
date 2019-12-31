3c63872c7da3ae2ec2c67b7ef07181919576c597
jcoshiro <joshiro@cisco.com>
2019-12-30 23:24:22 -0300

    Fix Formatting Issues

--- PageSpider.py
+++ PageSpider.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 
+
 def main(database: str, url_list_file: str):
     print("We are going to work with " + database)
     print("We are going to scan " + url_list_file)
@@ -14,4 +15,3 @@ if __name__ == "__main__":
     database_file = args.database
     input_file = args.input
     main(database=database_file, url_list_file=input_file)
-
